36f020298584a3ce1af9f95b84c7be727dfeb064
AbdulMoiz <pc16786abdulmoiz@gmail.com>
2025-09-24 12:57:17 +0500

    solve verify bug with google sign in

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'email',
         'password',
         'google_id',
+        'email_verified_at',
     ];
 
     protected $hidden = [
